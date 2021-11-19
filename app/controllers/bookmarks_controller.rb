aab05b89649571e7802c53af8c9a91a63bf6a2c4
Vincent Vignali <vincent.vignali@hotmail.fr>
2021-11-19 17:34:08 +0100

    fourth attempt

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -6,8 +6,19 @@ class BookmarksController < ApplicationController
   end
 
   def create
+    @bookmark = Bookmark.new(bookmark_params)
+    @list = List.find(params[:list_id])
+    @bookmark.list = @list
+    @bookmark.save
+    redirect_to list_path(@list)
   end
 
   def destroy
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
+  has_one_attached :photo
   has_many :bookmarks
   validates :title, presence: true
   validates :title, uniqueness: true
